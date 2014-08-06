b32fe125791165a816bcad20845c59fd6479f080
Christian Marie <christian@ponies.io>
2014-08-06 18:50:20 +1000

    Fix and then break the tests again, need to pass in a mock offset map and ensure that the pointers are correct

--- tests/UnitTests.hs
+++ tests/UnitTests.hs
@@ -20,16 +20,19 @@ import Test.Hspec
 import TimeStore.Algorithms
 import TimeStore.Core
 import TimeStore.Index
+import Control.Lens hiding (Simple, Index)
+import Data.ByteString.Builder
 
 main :: IO ()
 main =
     hspec . parallel $ do
         describe "algorithms" $ do
-            it "groups simple points only" groupSimpleOnly
+            it "groups simple points" groupSimple
+            it "groups extended points" groupExtended
 
 
-groupSimpleOnly :: IO ()
-groupSimpleOnly =
+groupSimple :: Expectation
+groupSimple =
     groupMixed simpleIndex extendedIndex simplePoints `shouldBe` expected
   where
     expected :: ( Map (Epoch, Bucket) SimpleWrite
@@ -68,8 +71,41 @@ groupSimpleOnly =
         , "\x00\x00\x00\x00\x00\x00\x00\x00"
         ]
 
+groupExtended :: Expectation
+groupExtended = do
+    let x@(_, _, ptrs, _, _)  = groupMixed simpleIndex extendedIndex extendedPoints
+    x `shouldBe` expected
+    print $ ptrs & traverse %~ (\(PointerWrite _ f) -> toLazyByteString $ f 0 Map.empty)
+  where
+    expected = ( mempty
+               , Map.fromList [ ((0, 0), bucket0_0)
+                              , ((0, 2), bucket0_2)
+                              ]
+               , Map.fromList [ ((0, 0), PointerWrite 24 undefined)
+                              , ((0, 2), PointerWrite 12 undefined)
+                              ]
+               , 2
+               , 2
+               )
+    bucket0_0 = fromString . concat $
+        [ "\x03\x00\x00\x00\x00\x00\x00\x00" -- Length
+        , "hai"                              -- Payload
+        , "\x05\x00\x00\x00\x00\x00\x00\x00" -- Length
+        , "there"                            -- Payload
+        ]
+
+    bucket0_2 = fromString . concat $
+        [ "\x04\x00\x00\x00\x00\x00\x00\x00" -- Length
+        , "pony"                             -- Payload
+        ]
+
 simplePoints :: ByteString
 simplePoints = vectorToByteString [Point 0 0 0, Point 2 2 0, Point 4 4 0, Point 8 8 0]
+
+extendedPoints :: ByteString
+extendedPoints =  vectorToByteString [Point 1 1 3] <> "hai"
+               <> vectorToByteString [Point 1 2 5] <> "there"
+               <> vectorToByteString [Point 3 1 4] <> "pony"
 
 simpleIndex :: Tagged Simple Index
 simpleIndex = Tagged [ (0, 4) :: (Epoch, Bucket)
